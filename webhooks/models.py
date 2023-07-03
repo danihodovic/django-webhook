fe27c60679e5b73b4a88479e1c8684a282d61723
Dani Hodovic <dani.hodovic@gmail.com>
2023-07-03 17:28:55 +0200

    wip

--- webhooks/models.py
+++ webhooks/models.py
@@ -1,1 +1,20 @@
-# from django.db import models
+from django.db import models
+
+from model_utils.models import TimeStampedModel
+
+
+class Webhook(TimeStampedModel):
+    url = URLField()
+    topics = ArrayField(base_field=models.CharField(max_length=TOPIC_MAX_LENGTH))
+    topics = models.ManyToManyField("webhooks.WebhookTopic")
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Webhook: id={self.id} url={self.url} topics={self.topics} "
+
+
+class WebhookTopic(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
--- webhooks/signals.py
+++ webhooks/signals.py
@@ -1,1 +1,6 @@
-# from webhooks import models
+from django.db.models.signals import post_delete, post_save
+
+
+# signal.connect(
+#     trigger_webhook, sender=model_cls, weak=False, dispatch_uid=dispatch_uid
+# )
