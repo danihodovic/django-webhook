255a3cf47f9d401ba809a797a165ea8da66c36cc
Andrew Dickinson <andrewjd@amazon.com>
2024-01-28 17:04:11 -0500

    Add support for datetime fields in user models

--- tests/models.py
+++ tests/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=30)
     email = models.EmailField()
+    join_date = models.DateField()
+    last_active = models.DateTimeField()
 
 
 class Country(models.Model):
