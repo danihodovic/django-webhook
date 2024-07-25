c8c330d8fa1461a2fa613fec343a8e3c4949fbb9
Etchegoyen Matthieu <etchegom@gmail.com>
2024-07-25 10:45:27 +0200

    explicitly set the default value of the automatic field to avoid a new migration being proposed

--- django_webhook/apps.py
+++ django_webhook/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 
 class WebhooksConfig(AppConfig):
     name = "django_webhook"
+    default_auto_field = "django.db.models.AutoField"
 
     def ready(self):
         from django.conf import settings
